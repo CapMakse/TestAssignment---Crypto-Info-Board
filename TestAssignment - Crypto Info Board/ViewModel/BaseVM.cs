3dd085022c9492fae59331e4e19efc33144b78ea
Makse3351 <pugachev.vv1401s@gmail.com>
2023-06-07 20:59:35 +0300

    Розпочав роботу над View та ViewModel

---
+++ TestAssignment - Crypto Info Board/ViewModel/BaseVM.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace TestAssignment___Crypto_Info_Board.ViewModel
+{
+    class BaseVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ TestAssignment - Crypto Info Board/ViewModel/WindowViewModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace TestAssignment___Crypto_Info_Board.ViewModel
+{
+    class WindowViewModel : BaseVM
+    {
+        private ICoinAPI _coinAPI;
+        private Page _currentPage;
+        public WindowViewModel()
+        {
+            _coinAPI = new CoinCapAPI();
+            _currentPage = new Pages.Main();
+        }
+        public Page CurrentPage
+        {
+            get { return _currentPage; }
+            set
+            {
+                _currentPage = value;
+                OnPropertyChanged("CurrentPage");
+            }
+        }
+    }
+}
